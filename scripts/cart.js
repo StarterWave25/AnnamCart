6dfce6919c24f40d7d4230b3f50bb4a895e1400f
Harsha <harshamahitha2079@gmail.com>
2025-03-29 08:49:48 +0530

    orderFood() almost done

--- scripts/cart.js
+++ scripts/cart.js
@@ -1,6 +1,14 @@
 const userMobile = sessionStorage.getItem('userMobile');
+let total, dummyTotal, response, restaurantData;
 async function getCart() {
-    let total = 0, dummyTotal = 0, response, restaurantData;
+
+    if (userMobile) {
+        const request = await fetch('data/data-profile.php?mode=getter');
+        const response = await request.json();
+        getAddress(response);
+    }
+
+    total = 0, dummyTotal = 0;
     let cartItems = await getQuantityStorage();
     if (await cartItems.length > 0) {
         response = await fetch(`data/data-restaurant.php?restaurant-id=${cartItems[0].restaurantId}`);
@@ -68,6 +76,8 @@ async function getCart() {
         }, 100);
 
     });
+
+
 
     function addCartHTML() {
         if (restaurantData) {
@@ -163,7 +173,7 @@ async function getCart() {
                     <h3>Pay on Delivery (Cash / UPI)</h3>
                 </div>
                 <div class="but-con">
-                    <button>Order Now</button>
+                    <button class="order-now-button">Order Now</button>
                 </div>
             </div>
         `;
@@ -177,7 +187,7 @@ async function getCart() {
             cartItemList.innerHTML = cartItemListHTML;
         } else {
             cartContainer.innerHTML =
-            `<div class="cart-empty-container">
+                `<div class="cart-empty-container">
                 <img src="img/empty-cart.png" class="cart-error-img">
                 <p class="item-list-error">Your cart is empty :(</p>
                 <a href="restaurants.html"><button>Browse Restaurants</button></a>
@@ -187,8 +197,8 @@ async function getCart() {
         }
     }
 
-    addCartHTML();
 
+    addCartHTML();
     /*
     let respondQuantity;
     async function getQuantity(itemId) {
@@ -208,7 +218,8 @@ async function getCart() {
 
         const response = await postRequest.json();
     } */
-
+    const orderBtn = document.querySelector('.order-now-button');
+    orderBtn.addEventListener('click', orderFood);
 }
 
 getCart();
@@ -250,3 +261,78 @@ async function loadingCart() {
 function getItemPrice(quantity, price) {
     return quantity * price;
 }
+
+const saveAddBtn = document.querySelector('.save-address');
+saveAddBtn.addEventListener('click', sendAddress);
+const changeAddBtn = document.querySelector('.change-address');
+changeAddBtn.addEventListener('click', changeAddress);
+const roomNo = document.querySelector('.address-room-no');
+const area = document.querySelector('.address-area');
+const landmark = document.querySelector('.address-landmark');
+
+async function sendAddress() {
+
+    const request = await fetch('data/data-profile.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ roomNo: roomNo.value, area: area.value, landmark: landmark.value })
+    });
+
+    const response = await request.json();
+    getAddress(response);
+}
+
+function getAddress(response) {
+    roomNo.value = response.room_no;
+    area.value = response.area;
+    landmark.value = response.landmark;
+
+    roomNo.setAttribute('readonly', 'true');
+    area.setAttribute('readonly', 'true');
+    landmark.setAttribute('readonly', 'true');
+
+    saveAddBtn.style.pointerEvents = 'none';
+    saveAddBtn.style.opacity = '.3';
+
+    changeAddBtn.style.pointerEvents = 'all';
+    changeAddBtn.style.opacity = '1';
+}
+
+async function changeAddress() {
+    changeAddBtn.style.pointerEvents = 'none';
+    changeAddBtn.style.opacity = '0.3';
+
+    roomNo.removeAttribute('readonly', 'true');
+    area.removeAttribute('readonly', 'true');
+    landmark.removeAttribute('readonly', 'true');
+
+    roomNo.focus();
+    saveAddBtn.style.pointerEvents = 'all';
+    saveAddBtn.style.opacity = '1';
+}
+
+
+function generateOrderID() {
+    const timestamp = Date.now().toString().slice(-6);
+    const randomStr = Math.random().toString(36).substring(2, 6).toUpperCase();
+    return `ORD${userMobile.slice(-4)}${randomStr}${timestamp}`;
+}
+
+
+
+async function orderFood() {
+    const cart = await getQuantityStorage();
+    const orderId = generateOrderID();
+    const request = await fetch('data/data-orders.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ orderId, cart, total, dummyTotal, items: cart.length, resId: cart[0].restaurantId })
+    });
+    const response = await request.json();
+    console.log(response);
+}
+
