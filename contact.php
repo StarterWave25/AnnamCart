2044bf7299c4e44199be0703c8c9d1fb8d4a6c1a
Harsha <harshamahitha2079@gmail.com>
2025-05-04 12:21:54 +0530

    testimonals cards front-end

--- contact.php
+++ contact.php
@@ -1,23 +1,20 @@
 <?php
-  $servername = "localhost";  
-  $username = "root";         
-  $password = "";             
-  $database = "food_delivery";
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "food_delivery";
 
-  $conn = mysqli_connect($servername,$username,$password,$database);
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-  $name = $_POST["user-name"];
-  $mobile_number = $_POST["mobilenumber"];
-  $issue = $_POST["issue"];
-  $sql = "insert into contact values('$name','$mobile_number','$issue')";
-  
-  $result = mysqli_query($conn,$sql);
-  
- echo " <html>
+$name = $_POST["user-name"];
+$mobile_number = $_POST["mobilenumber"];
+$issue = $_POST["issue"];
+$sql = "insert into contact values('$name','$mobile_number','$issue')";
+
+$result = mysqli_query($conn, $sql);
+
+echo " <html>
       <body>
         <h1>we will contact you in few mintiues</h1>
       </body>
-  </html>"
-  
-
-?>
+  </html>";
